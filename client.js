a008c70ac3ef471e1d80021f0d74a4c952c03703
Henri Lindqvist <henri.lindqvist@hotmail.com>
2020-12-03 17:13:27 +0200

    on

--- client.js
+++ client.js
@@ -1,21 +1,63 @@
+//Made by Pizzakeitto with the help of jeesusmies
+//I have no more braincells left
+
 const net = require('net');
+const readline = require('readline');
 
-const prompt = require('prompt-sync')();
+var name;
+var isReady = false;
 
-async function chat(){
-    let data = prompt('> ');
-    client.write(data);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.on('SIGINT', () => {
+    console.log('Nah m8 do .exit instead :)');
+    //rl.question('Are you sure you want to quit? ', (ans) => {
+    //    if(ans.match(/^y(es)?$/i)) {
+    //        rl.close();
+    //        client.end();
+    //    }
+    //})
+    //TODO: Get this working lol
+})
+
+function chat(){
+    rl.question('> ', (ans) => {
+        if(ans.toLowerCase() == '.exit') {
+            client.write(`${name} has left the chat`)
+            client.end();
+            return;
+        }
+        process.stdout.clearLine(0, () => { });
+        client.write(`${name}: ${ans}`);
+        setTimeout(() => {
+            chat()
+        }, 50);
+    })
 }
 
 const client = net.connect(3000, 'localhost', () => {
-    console.log('i think ima conec ted');
-    //chat();
+    setTimeout(() => {
+        rl.question('Oi what you be called? ', (ans) => {
+            name = ans;
+            client.write(`${name} has joined the chat`);
+            isReady = true;
+            setTimeout(() => { chat(); }, 500)
+        })
+    }, 50)
 })
 
 client.on('data', (data) => {
+    process.stdout.clearLine(0, () => { });
+    process.stdout.cursorTo(0, () => { });
     console.log(data.toString());
-    chat();
+    if(isReady){
+        chat();
+    }
 })
 client.on('end', () => {
-    console.log('Discone cted');
+    console.log('Disconnected!');
+    rl.close();
 })
--- server.js
+++ server.js
@@ -1,21 +1,35 @@
+//Made by Pizzakeitto with the help of jeesusmies
+//I have no more braincells left
+
 const net = require('net');
+let sockets = [];
+
+function arrayRemove(arr, value){
+    return arr.filter(function(ele){ 
+        return ele != value; 
+    });
+}
 
 const server = net.createServer((socket) => {
     console.log('Someone connected!');
+    sockets.push(socket);
     socket.on('end', () =>{
         console.log('Someone disconnected!');
+        arrayRemove(sockets, socket);
     })
     socket.on('error', (err) => {
         //throw(err);
         console.log('Ignoring error: ' + err);
     })
+    let count = server.connections;
     socket.write('Welcome to shitty chatroom!\n');
-    var count = server.connections;
-    socket.write('Youre here with ' + count + ' other people.')
+    socket.write('Youre here with ' + count + ' other people.\n')
+    socket.write('Oh btw dont do CTRL C, things will break!\n')
     //socket.pipe(socket);
     socket.on('data', (data) => {
-        console.log(data);
-        //socket.write(data);
+        sockets.forEach(socket => {
+            socket.write(data);
+        });
     })
 })
 
